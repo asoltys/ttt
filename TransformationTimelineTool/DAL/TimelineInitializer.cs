97db51e2b9cb92b4872e1f4540e9f87b99fc70a4
Mathieu Wong-Rose <mathieu.wong-rose@pwgsc.gc.ca>
2015-11-16 15:16:34 -0800

    Changes to Initializer Event model Initiative details view

--- TransformationTimelineTool/DAL/TimelineInitializer.cs
+++ TransformationTimelineTool/DAL/TimelineInitializer.cs
@@ -7,7 +7,7 @@ using TransformationTimelineTool.Models;
 
 namespace TransformationTimelineTool.DAL
 {
-    public class TimelineInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<TimelineContext>
+    public class TimelineInitializer : System.Data.Entity.DropCreateDatabaseAlways<TimelineContext>
     {
         protected override void Seed(TimelineContext context)
         {
@@ -15,7 +15,8 @@ namespace TransformationTimelineTool.DAL
             {
                 new Region {NameShort = "pac", Name = "Pacific" },
                 new Region {NameShort = "wst", Name = "Western" },
-                new Region {NameShort = "ont", Name = "Ontario" }
+                new Region {NameShort = "wst", Name = "Western" },
+                new Region {NameShort = "que", Name = "Quebec" }
             };
 
             regions.ForEach(r => context.Regions.Add(r));
@@ -29,6 +30,26 @@ namespace TransformationTimelineTool.DAL
             };
 
             branches.ForEach(b => context.Branches.Add(b));
+            context.SaveChanges();
+
+            var initiatives = new List<Initiative>
+            {
+                new Initiative {Name = "Defense Procurement Strategy", Description = "Descp 1", StartDate = DateTime.Parse("2013-12-1"), EndDate = DateTime.Parse("2020-3-31") },
+                new Initiative {Name = "Case and Client Relationship Management", Description = "Descp 2", StartDate = DateTime.Parse("2013-12-1"), EndDate = DateTime.Parse("2020-3-31") },
+
+            };
+
+            initiatives.ForEach(i => context.Initiatives.Add(i));
+            context.SaveChanges();
+
+            var events = new List<Event>
+            {
+               new Event {InitiativeID = 1, Date = DateTime.Parse("2015-07-31"), Text = "Launch of SMART Procurement", Hover = "Launch of SMART Procurement" },
+               new Event {InitiativeID = 1, Date = DateTime.Parse("2015-06-28"), Text = "Launch of SMART Procurement LEAN Pilot", Hover = "Launch of SMART Procurement LEAN Pilot" },
+               new Event {InitiativeID = 2, Date = DateTime.Parse("2015-07-31"), Text = "Online E-Learning courses offered", Hover = "Online E-Learning courses offered" }
+            };
+
+            events.ForEach(e => context.Events.Add(e));
             context.SaveChanges();
         }
     }
--- TransformationTimelineTool/Models/Event.cs
+++ TransformationTimelineTool/Models/Event.cs
@@ -8,6 +8,7 @@ namespace TransformationTimelineTool.Models
     public class Event
     {
         public int ID { get; set; }
+        public int InitiativeID { get; set; }
         public DateTime Date { get; set; }
         public String Text { get; set; }
         public String Hover { get; set; }
