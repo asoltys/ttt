cd577525c982ded38bf5e6d3339a18e85e775a22
Mathieu Wong-Rose <mathieu.wong-rose@pwgsc.gc.ca>
2015-11-26 15:48:51 -0800

    default home page

--- TransformationTimelineTool/App_Start/RouteConfig.cs
+++ TransformationTimelineTool/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TransformationTimelineTool
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Initiatives", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
