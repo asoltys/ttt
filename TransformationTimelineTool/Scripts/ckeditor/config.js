072aeeedfb09dd34929d4a51f1d7135e378aff57
Mathieu Wong-Rose <mathieu.wong-rose@pwgsc.gc.ca>
2015-12-09 16:25:02 -0800

    Rich text editor textareas

---
+++ TransformationTimelineTool/Scripts/ckeditor/config.js
@@ -0,0 +1,25 @@
+/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	config.toolbarGroups = [
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		'/',
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
+
+	config.removeButtons = 'Source,Save,NewPage,Preview,Print,Templates,Form,Checkbox,Radio,TextField,Select,Textarea,Button,ImageButton,HiddenField,Blockquote,CreateDiv,BidiLtr,BidiRtl,Language,Image,Styles,TextColor,Maximize,About,ShowBlocks,BGColor,Flash,Table,Format,Iframe,PageBreak,SpecialChar,Smiley,HorizontalRule,JustifyLeft,JustifyCenter,JustifyRight,JustifyBlock,Cut,Copy,Paste,PasteText,PasteFromWord';
+};
