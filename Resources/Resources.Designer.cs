064bd0c79b7de526ddc4d12e5624fd0a04dae4ad
Mathieu Wong-Rose <mathieu.wong-rose@pwgsc.gc.ca>
2016-01-13 11:48:35 -0800

    Fixed some more merge confs Added font-weight bold to labels throughout Custom css added to template

--- Resources/Resources.Designer.cs
+++ Resources/Resources.Designer.cs
@@ -151,6 +151,15 @@ namespace Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Description.
+        /// </summary>
+        public static string Description {
+            get {
+                return ResourceManager.GetString("Description", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Deselect.
         /// </summary>
         public static string Deselect {
