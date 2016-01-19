46f5dfaca553404923ff6afb6ca6d7c897e9147a
Ryan Seo <Ryan.Seo@pwgsc.gc.ca>
2016-01-19 11:51:20 -0800

    Changing language now directs users to the Index of the same controller

--- TransformationTimelineTool/Controllers/HomeController.cs
+++ TransformationTimelineTool/Controllers/HomeController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
+using TransformationTimelineTool.Helpers;
 
 namespace TransformationTimelineTool.Controllers
 {
@@ -57,10 +60,16 @@ namespace TransformationTimelineTool.Controllers
         public ActionResult SetCulture(string culture)
         {
             RouteData.Values["culture"] = culture;
+            string Referrer = Request.UrlReferrer.ToString();
 
+            var request = new HttpRequest(null, Referrer, null);
+            var response = new HttpResponse(new StringWriter());
+            var httpContext = new HttpContext(request, response);
+            var routeData = RouteTable.Routes.GetRouteData(new HttpContextWrapper(httpContext));
+            var values = routeData.Values;
 
-            return RedirectToAction("Index","Initiatives");
-            //return Redirect(Request.UrlReferrer.ToString());
+            string PreviousController = values["controller"].ToString();
+            return RedirectToAction("Index", PreviousController);
         }
     }
 }
