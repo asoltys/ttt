0006a78e6a7225a153de9167868c084063fead53
Ryan Seo <Ryan.Seo@pwgsc.gc.ca>
2016-03-02 14:09:57 -0800

    prepare a function for mail body

--- TransformationTimelineTool/Helpers/ChangeNotify.cs
+++ TransformationTimelineTool/Helpers/ChangeNotify.cs
@@ -11,22 +11,43 @@ namespace TransformationTimelineTool.Helpers
 {
     public class ChangeNotify : IJob
     {
-        List<Edit> ChangedEdits;
-        List<Impact> ChangedImpact;
-        List<Initiative> ChangedInitiatives;
+        private static List<Edit> ChangedEdits;
+        private static List<Impact> ChangedImpacts;
+        private static List<Initiative> ChangedInitiatives;
         TimelineContext db = new TimelineContext();
 
         public void Execute(IJobExecutionContext context)
         {
-            var edits = db.Edits.Where(e => e.Edited == true).ToList();
-            var impacts = db.Impacts.Where(i => i.Edited == true).ToList();
-            var initiatives = db.Initiatives.Where(i => i.Edited == true).ToList();
+            ChangedEdits = db.Edits.Where(e => e.Edited == true).ToList();
+            ChangedImpacts = db.Impacts.Where(i => i.Edited == true).ToList();
+            ChangedInitiatives = db.Initiatives.Where(i => i.Edited == true).ToList();
 
             List<string> Recipients = GetRecipients();
+            string MailSubject = "There has been some changes to events you subscribed.";
+            string MailBody = GetMailBody();
+
             foreach (var recipient in Recipients)
             {
-                Utils.SendMail(recipient, "ChangeNotify test", "test");
+                Utils.SendMail(recipient, MailSubject, MailBody);
             }
+        }
+
+        private static string GetMailBody()
+        {
+            string Body = "Following list has been changed since last week. <br>";
+            foreach (var edit in ChangedEdits)
+            {
+                Body += "";
+            }
+            foreach (var initiative in ChangedInitiatives)
+            {
+                Body += "The initiative " + initiative.NameE + " has changed. <br>";
+            }
+            foreach (var impact in ChangedImpacts)
+            {
+                Body += "The impact for " + impact.Initiative.NameE + " has changed. <br>";
+            }
+            return Body;
         }
 
         private static List<string> GetRecipients()
@@ -35,7 +56,7 @@ namespace TransformationTimelineTool.Helpers
             List<string> EmailList = new List<string>();
 
             // Mass email send testing can be done here; just increase conditionals
-            for (var i = 0; i < 50; i++)
+            for (var i = 0; i < 1; i++)
             {
                 EmailList.Add(email);
             }
