40bf459853db5c2a0592010522bdbc49347be989
Ryan Seo <Ryan.Seo@pwgsc.gc.ca>
2016-03-02 11:04:56 -0800

    mass mail test

--- TransformationTimelineTool/Helpers/ChangeNotify.cs
+++ TransformationTimelineTool/Helpers/ChangeNotify.cs
@@ -20,7 +20,25 @@ namespace TransformationTimelineTool.Helpers
             var edits = db.Edits.Where(e => e.Edited == true).ToList();
             var impacts = db.Impacts.Where(i => i.Edited == true).ToList();
             var initiatives = db.Initiatives.Where(i => i.Edited == true).ToList();
-            Utils.log(edits);
+
+            List<string> Recipients = GetRecipients();
+            foreach (var recipient in Recipients)
+            {
+                Utils.SendMailAsync(recipient, "ChangeNotification Test", "");
+            }
+        }
+
+        private static List<string> GetRecipients()
+        {
+            var email = "Ryan.Seo@pwgsc.gc.ca";
+            List<string> EmailList = new List<string>();
+
+            // Mass email send testing can be done here; just increase conditionals
+            for (var i = 0; i < 10; i++)
+            {
+                EmailList.Add(email);
+            }
+            return EmailList;
         }
 
     }
