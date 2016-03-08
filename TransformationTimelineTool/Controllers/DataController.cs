7614d8945ac8f845956d3a290321b7f12c88720e
Ryan Seo <Ryan.Seo@pwgsc.gc.ca>
2016-03-07 16:48:10 -0800

    add regions & branches production data

--- TransformationTimelineTool/Controllers/DataController.cs
+++ TransformationTimelineTool/Controllers/DataController.cs
@@ -10,17 +10,31 @@ namespace TransformationTimelineTool.Controllers
     public class DataController : Controller
     {
 
-        [Route("initiative-eng")]
+        [Route("initiatives-eng")]
         public ActionResult ReturnMockDataEng()
         {
             var path = "~/Scripts/MockData/data-eng.json";
             return File(path, "application/json");
         }
 
-        [Route("initiative-fra")]
+        [Route("initiatives-fra")]
         public ActionResult ReturnMockDataFra()
         {
             var path = "~/Scripts/MockData/data-fra.json";
+            return File(path, "application/json");
+        }
+
+        [Route("regions")]
+        public ActionResult ReturnRegions()
+        {
+            var path = "~/Scripts/MockData/regions.json";
+            return File(path, "application/json");
+        }
+
+        [Route("branches")]
+        public ActionResult ReturnBranches()
+        {
+            var path = "~/Scripts/MockData/branches.json";
             return File(path, "application/json");
         }
     }
