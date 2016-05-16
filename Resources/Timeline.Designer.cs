b8b3544d86ad1d7161e4e67bc6bdc4b00754f27e
jeholmes <jonathan.holmes@pwgsc-tpsgc.gc.ca>
2016-05-16 15:54:26 -0700

    Updated the intial filter message for the accessible page according to comment in bug #7086.

--- Resources/Timeline.Designer.cs
+++ Resources/Timeline.Designer.cs
@@ -97,7 +97,7 @@ namespace Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to &lt;p&gt;This timeline tool has been designed to provide you with a list of the current transformation and Blueprint 2020 initiatives based on the area and the branch that you have chosen. The tool will display a re-sorted list of initiatives that have been organized based on the degree to which they will change the way you do your work.&lt;/p&gt; &lt;p&gt;Within your displayed results, click on the name of the initiative to read more about what it is and how it generally impacts employees. You can also click on the Red Dot  [rest of string was truncated]&quot;;.
+        ///   Looks up a localized string similar to &lt;p&gt;This timeline tool has been designed to provide you with a list of the current transformation and Blueprint 2020 initiatives based on the area and the branch that you have chosen. The tool will display a re-sorted list of initiatives that have been organized based on the degree to which they will change the way you do your work.&lt;/p&gt; &lt;p&gt;Within your displayed results, click on the name of the initiative to read more about what it is and how it generally impacts employees. Learn more about the milestones fo [rest of string was truncated]&quot;;.
         /// </summary>
         public static string InitialFilterAccessible {
             get {
