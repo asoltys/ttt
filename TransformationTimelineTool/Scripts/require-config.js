f0df2149e0d76833b20db076b32f360d818fcd1e
Ryan Seo <Ryan.Seo@pwgsc.gc.ca>
2016-06-27 10:29:38 -0700

    restore require config (this is dev config)

---
+++ TransformationTimelineTool/Scripts/require-config.js
@@ -0,0 +1,19 @@
+﻿requirejs.config({
+	// load any script from lib directory
+	enforceDefine: true,
+	baseUrl: 'Scripts/lib',
+	waitSeconds: 15,
+	paths: {
+        'jquery': 'jquery-v2',
+		'jquery-ui': 'jquery-ui.min'
+	},
+	map: {
+		'*': { 'jquery': 'jquery-private' },
+		'jquery-private': { 'jquery': 'jquery' }
+	},
+	shim: {
+		'jquery-ui': {
+			deps: ['jquery-private']
+		} 
+	}
+});
